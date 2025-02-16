2a031b15431b9dc72e1e91c67997aa3a69cdae14
alpimp <alannapimple306@gmail.com>
2025-02-17 01:30:18 +0300

    Added country

--- functions/click.js
+++ functions/click.js
@@ -5,7 +5,7 @@ export async function onRequest(context) {
   const ref = request.headers.get('referer') || '';
   const country_code = request.cf.country || 'Unknown';
   const tz = request.cf.timezone || 'Unknown';
-  const asn = request.cf.asn || 'Unknown';
+  const asn = request.cf.asn || "";
   const pparams = url.searchParams;
 
   console.log('ref is ' + ref);
@@ -28,7 +28,7 @@ export async function onRequest(context) {
 
   console.log('Submitting Form Results...');
 
-  await handleFormSubmit(ref, ip, dt, tz, country_code, env);
+  await handleFormSubmit(ref, ip, dt, tz, asn, country_code, env);
 
   let destinationURL = env.TRACKER;
   if (pparams && pparams.toString()) {
@@ -57,7 +57,7 @@ async function createNotionPage(body, env) {
   });
 }
 
-async function handleFormSubmit(rr, i, d, tz, country_code, env) {
+async function handleFormSubmit(rr, i, d, tz, asn, country_code, env) {
   try {
     const requestBody = {
       parent: {
@@ -65,7 +65,7 @@ async function handleFormSubmit(rr, i, d, tz, country_code, env) {
       },
       properties: {
         Display: { title: [{ text: { content: "BL" } }] },
-        Touch: { rich_text: [{ text: { content: "BL" } }] },
+        Touch: { rich_text: [{ text: { content: asn } }] },
         Lang: { rich_text: [{ text: { content: country_code } }] },
         TZ: { rich_text: [{ text: { content: tz } }] },
         "FDB type": { multi_select: [{ name: "BL" }] },
