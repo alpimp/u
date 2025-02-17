30d5970c87526687f6f167cfd5c0f0ab424ecc3a
alpimp <alannapimple306@gmail.com>
2025-02-17 04:12:36 +0300

    added accel

--- functions/click.js
+++ functions/click.js
@@ -1,18 +1,13 @@
 export async function onRequest(context) {
   const { request, env } = context;
 
-  const formData = await request.formData();
   const url = new URL(request.url);
   const ref = request.headers.get('referer') || '';
-  //const ref = formData?.get("demo_val") ?? 'Unknown';
+  const ref = request.headers.get('referer') || '';
   const country_code = request.cf?.country ?? 'Unknown';
   const tz = request.cf?.timezone ?? 'Unknown';
   const asn = request.cf?.asn ?? 'Unknown';
   const pparams = url.searchParams;
-  const accel = formData.get("alpha_val") || 'Unknown';
-  const touch = formData.get("touch") || 'Unknown';
-  const display = formData.get("display") || 'Unknown';
-  const ua = formData.get("get_ua") || 'Unknown';
 
   console.log('ref is ' + ref);
   console.log('my params are ' + pparams);
@@ -34,7 +29,7 @@ export async function onRequest(context) {
 
   console.log('Submitting Form Results...');
 
-  await handleFormSubmit(ref, ip, dt, tz, asn, country_code, accel, touch, display, ua, env);
+  await handleFormSubmit(ref, ip, dt, tz, asn, country_code, env);
 
   let destinationURL = env.TRACKER;
   if (pparams && pparams.toString()) {
@@ -63,23 +58,22 @@ async function createNotionPage(body, env) {
   });
 }
 
-async function handleFormSubmit(rr, i, d, tz, asn, country_code, accel, touch, display, ua, env) {
+async function handleFormSubmit(rr, i, d, tz, asn, country_code, env) {
   try {
     const requestBody = {
       parent: {
         database_id: env.DB2_ID,
       },
       properties: {
-        Display: { title: [{ text: { content: display } }] },
-        Touch: { rich_text: [{ text: { content: touch } }] },
+        Display: { title: [{ text: { content: "BL" } }] },
+        Touch: { rich_text: [{ text: { content: "BL" } }] },
         Lang: { rich_text: [{ text: { content: country_code } }] },
         TZ: { rich_text: [{ text: { content: tz } }] },
         "FDB type": { rich_text: [{ text: { content: String(asn) } }] },
         Messaga: { rich_text: [{ text: { content: d } }] },
         Location: { rich_text: [{ text: { content: rr } }] },
         IP: { rich_text: [{ text: { content: i } }] },
-        Accel: { rich_text: [{ text: { content: accel } }] },
-        UA: { rich_text: [{ text: { content: ua } }] },
+        Accel: { rich_text: [{ text: { content: "Waiting.." } }] },
       },
     };
 
