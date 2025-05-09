6a67d4975200903da0d662363e3d7448f00b401b
alpimp <alannapimple306@gmail.com>
2025-05-09 10:10:21 +0300

    tracker

--- functions/click.js
+++ functions/click.js
@@ -42,8 +42,8 @@ try {
 
   await handleFormSubmit(ref, ip, dt, tz, asn, country_code, accel, touch, display, ua, env);
 
-//  let destinationURL = env.TRACKER;
-  let destinationURL ='https://18dbne.mcgo2.com/click';
+  let destinationURL = env.TRACKER;
+//  let destinationURL ='https://18dbne.mcgo2.com/click';
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // uncomment if search params have to be appended to the destination url 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
